e15fd45309075415a092d1a2c1e2d118f430be79
rkiller <rogerkiller@gmail.com>
2019-06-09 17:58:33 -0700

    Oauth2 Azure 2.0 Migration

--- lib/omniauth/strategies/azure_oauth2.rb
+++ lib/omniauth/strategies/azure_oauth2.rb
@@ -12,7 +12,8 @@ module OmniAuth
       # AD resource identifier
       #option :resource, '00000002-0000-0000-c000-000000000000'
       option :version, 'v2.0/'
-      option :scope, 'https://graph.windows.net/user.read'
+      option :scope, 'email profile https://graph.windows.net/user.read'
+      option :response_type, 'id_token'
 
       # tenant_provider must return client_id, client_secret and optionally tenant_id and base_azure_url
       args [:tenant_provider]
@@ -32,6 +33,7 @@ module OmniAuth
           provider.respond_to?(:base_azure_url) ? provider.base_azure_url : BASE_AZURE_URL
 
         options.authorize_params = provider.authorize_params if provider.respond_to?(:authorize_params)
+        options.authorize_params.response_type = provider.response_type if provider.respond_to?(:response_type) && provider.response_type
         options.authorize_params.domain_hint = provider.domain_hint if provider.respond_to?(:domain_hint) && provider.domain_hint
         options.authorize_params.prompt = request.params['prompt'] if defined? request && request.params['prompt']
         options.authorize_params.scope = provider.scope if provider.respond_to?(:scope) && provider.scope
@@ -59,11 +61,10 @@ module OmniAuth
         }
       end
 
-      def token_params
+      #def token_params
       #  azure_resource = request.env['omniauth.params'] && request.env['omniauth.params']['azure_resource']
       #  super.merge(resource: azure_resource || options.resource)
-        super.merge(response_type: "id_token")
-      end
+      #end
 
       def callback_url
         full_host + script_name + callback_path
